7bd3902180ca78055dab265ff5d8fbb22c15cebe
epkWarner <evan.k.warner@gmail.com>
2018-11-14 18:14:59 -0500

    plzzz wurk

--- src/app/shared/interceptors/intercept.url.ts
+++ src/app/shared/interceptors/intercept.url.ts
@@ -1,16 +1,25 @@
-import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler } from "@angular/common/http";
+import {
+    HttpInterceptor,
+    HttpRequest,
+    HttpEvent,
+    HttpHandler
+} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { APIURL } from "../../../environments/environment.prod";
 
-const base = 'http://127.0.0.1:5000'
+const base = `${APIURL}`;
 
 @Injectable()
 export class UrlInterceptor implements HttpInterceptor {
     constructor() { }
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
         req = req.clone({
             url: base + req.url
-        })
-        return next.handle(req)
+        });
+        return next.handle(req);
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
   production: true
 };
+
+export let APIURL = '';
+
+switch (window.location.hostname) {
+  // this is the deployed angular application
+  case 'nightbuzz-plzzz-wurk.herokuapp.com':
+    // this is the full url of your deployed API
+    APIURL = 'https://nitebuzz2018v1.herokuapp.com'
+    break;
+  default:
+    // this is the local host name of your API
+    APIURL = 'https://nitebuzz2018v1.herokuapp.com';
+}
